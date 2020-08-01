182710eae61102146ffcf2a5c412817869c7ce66
David Fernando Ciro Zapata <inforfercho@gmail.com>
2020-08-01 13:57:45 -0500

    Add project files.

---
+++ FontIconsDemoTest/FontIconsDemoTest/ContenConstanst.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FontIconsDemoTest
+{
+    public static class ContenConstanst
+    {
+        public const string Warning = "\ue800";
+        public const string Map = "\ue801";
+        public const string Location = "\ue802";
+        public const string MenuOutline = "\ue803";
+        public const string BlockOutline = "\ue804";
+        public const string Mic = "\ue805";
+    }
+}
